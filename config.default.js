63dadd5821876722d0064d2c746929a6f66e210c
ZHAO Xudong <zxdong@gmail.com>
2018-11-29 10:48:39 +0800

    init

---
+++ config.default.js
@@ -0,0 +1,41 @@
+const os = require('os')
+const extend = require('recursive-assign')
+let config = {
+
+  //dev related
+  devCPUCount: os.cpus().length,
+  devPort: 8020,
+
+  //build options
+  minimize: false,
+
+  //congfigs to build app
+
+  //ringcentral config
+  ringCentralConfigs: {
+    clientID: '',
+    appServer: ''
+  },
+
+  //for third party related
+  thirdPartyConfigs: {
+    apiServer: 'https://api.insightly.com/v3.0',
+    showCallLogSyncForm: true,
+    serviceName: 'Insightly'
+  }
+
+}
+
+try {
+  extend(config, require('./config.js'))
+} catch (e) {
+  if (e.stack.includes('Cannot find module \'./config.js\'')) {
+    console.warn('no custom config file, it is ok, but you can use "cp config.sample.js config.js" to create one')
+  } else {
+    console.log(e)
+  }
+}
+module.exports = config
+
+
+
